cb159d527db51852958883c49d7f5eb5d6834673
Azizul Hasan <43531896+azizulhasan@users.noreply.github.com>
2022-02-04 12:34:20 +0600

    Dashboard setup completed.

--- client/src/components/front/portfolio/Home.js
+++ client/src/components/front/portfolio/Home.js
@@ -6,7 +6,7 @@ import "./assets/vendor/boxicons/css/boxicons.min.css"
 import "./assets/vendor/glightbox/css/glightbox.min.css"
 import "./assets/vendor/swiper/swiper-bundle.min.css"
 import "./assets/css/style.css"
-import {addScripts}  from '../../context/utilities'
+import {addScripts}  from '../../Context/utilities'
 // import 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i'
 export default function Home() {
   useEffect(()=>{
